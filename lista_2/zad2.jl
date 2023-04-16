94f13347deb693edff6fc1a2118a83bc103874a9
AHerda <adrianherda@gmail.com>
2023-04-16 13:01:13 +0200

    Zadanie 2 w połowie

--- lista_2/zad2.jl
+++ lista_2/zad2.jl
@@ -12,16 +12,39 @@ for i in 1:n, j in i:n
     time[i, j] = rand(rng, 1:(1.5 * n))
     cost[i, j] = rand(rng, 1:(1.5 * n))
 end
-println(time)
-println(cost)
+
+for i in 1:n, j in 1:i
+    time[i, j] = typemax(time[i, j])
+    cost[i, j] = typemax(time[i, j])
+end
 
 time_cap = n * 1.5
 
 town_start = 1
-town_stop = 10
+town_stop = n
 
-model = Model(HigHS.Optimizer)
+function valid_path(x, start, stop)
+    node = start
+    while sum(x[node, :]) == 1
+        for i in 1:n
+            if x[node, i] == 1
+                node = i
+            end
+        end
+    end
+    if node == stop
+        return true
+    else
+        return false
+    end
+end
 
-@variable(model, )
+model = Model(HiGHS.Optimizer)
+
+@variable(model, var[1:n, 1:n], Bin)
+@constraint(model, [i in 1:n], sum(var[:, i]) <= 1)
+@constraint(model, [i in 1:n], sum(var[i, :]) <= 1)
+@constraint(model, sum(var'*time) <= time_cap)
+@objective(model, Min, sum(var'*cost))
 
 return time
