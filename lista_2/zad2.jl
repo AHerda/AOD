f0988b5e41d9103787e8fbb5ee3e6c1d244d059b
AHerda <adrianherda@gmail.com>
2023-04-17 04:11:47 +0200

    Zadanie 2 nadal w połowie

--- lista_2/zad2.jl
+++ lista_2/zad2.jl
@@ -8,7 +8,7 @@ rng = MersenneTwister(1234);
 time = zeros(n, n)
 cost = zeros(n, n)
 
-for i in 1:n, j in i:n
+for i in 1:n, j in 1:(i + 4 - ((i + 4) % n))
     time[i, j] = rand(rng, 1:(1.5 * n))
     cost[i, j] = rand(rng, 1:(1.5 * n))
 end
@@ -21,30 +21,26 @@ end
 time_cap = n * 1.5
 
 town_start = 1
-town_stop = n
+town_end = n
 
-function valid_path(x, start, stop)
-    node = start
-    while sum(x[node, :]) == 1
-        for i in 1:n
-            if x[node, i] == 1
-                node = i
-            end
-        end
-    end
-    if node == stop
-        return true
-    else
-        return false
-    end
-end
 
 model = Model(HiGHS.Optimizer)
 
-@variable(model, var[1:n, 1:n], Bin)
-@constraint(model, [i in 1:n], sum(var[:, i]) <= 1)
-@constraint(model, [i in 1:n], sum(var[i, :]) <= 1)
+@variable(model, 0 <= var[1:n, 1:n] <= 1, Bin)
+
+# Nie może być połączenia tam gdzie cena == 0, połączenie nie istnieje
+@constraint(model, [i in 1:n, j in 1:n, cost[i, j] == 0], var[i, j] == 0)
+
+# Dla punktów tranzytowych
+@constraint(model, [i in 1:n, i != town_start, i != town_end], sum(var[i, :]) == sum(var[:, i]))
+
+# Blinas startowego punktu == 1, końcowego punktu == -1
+@constraint(model, sum(var[town_start, :]) - sum(var[:, town_start]) == 1)
+@constraint(model, sum(var[town_end, :]) - sum(var[:, town_end]) == -1)
+
+#czas nie może przekraczać wyznaczonego ograniczenia
 @constraint(model, sum(var'*time) <= time_cap)
+
 @objective(model, Min, sum(var'*cost))
 
 return time
