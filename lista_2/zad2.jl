4208c0819f7296d892091b411b959a515e45e056
AHerda <adrianherda@gmail.com>
2023-04-18 16:21:50 +0200

    Skonczone zadanie 2

--- lista_2/zad2.jl
+++ lista_2/zad2.jl
@@ -17,7 +17,7 @@ for i in 1:n, j in 1:min(n, i + 5)
 end
 
 
-time_cap = n * 4
+time_cap = n * 1.5
 
 town_start = 1
 town_end = n
@@ -29,7 +29,7 @@ model = Model(HiGHS.Optimizer)
 
 @variable(model, var[1:n, 1:n], Bin)
 # Nie może być połączenia tam gdzie cena == 0, połączenie nie istnieje
-@constraint(model, [i in 1:n, j in 1:n, cost[i, j] == 0], var[i, j] == 0)
+@constraint(model, [i in 1:n, j in 1:n; cost[i, j] == 0], var[i, j] == 0)
 # Wyjście i wejście z każdego node'a może byctylko jedno
 @constraint(model, [i in 1:n], sum(var[i, :]) <= 1)
 @constraint(model, [i in 1:n], sum(var[:, i]) <= 1)
@@ -45,4 +45,5 @@ solution_summary(model)
 
 println("\nKoszt najkrótszej drogi: ", objective_value(model))
 println("Czas najkrótszej drogi: ", value(sum(time .* var)))
-println("Ścieżka: ", value.(var))
+println("Ścieżka:")
+display(value.(var))
