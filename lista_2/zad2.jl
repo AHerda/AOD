ed73989d66b293e4c9088c771779440b864f9a43
AHerda <adrianherda@gmail.com>
2023-04-18 16:00:00 +0200

    Zadanie drugie poprawione

--- lista_2/zad2.jl
+++ lista_2/zad2.jl
@@ -11,17 +11,13 @@ time = zeros(n, n)
 cost = zeros(n, n)
 bilance = zeros(n)
 
-for i in 1:n, j in 1:(i + 4 - ((i + 4) % n))
-    time[i, j] = rand(rng, 1:(1.5 * n))
-    cost[i, j] = rand(rng, 1:(1.5 * n))
+for i in 1:n, j in 1:min(n, i + 5)
+    time[i, j] = rand(rng, 1:10)
+    cost[i, j] = rand(rng, 1:10)
 end
 
-for i in 1:n, j in 1:i
-    time[i, j] = typemax(time[i, j])
-    cost[i, j] = typemax(time[i, j])
-end
 
-time_cap = n * 1.5
+time_cap = n * 4
 
 town_start = 1
 town_end = n
@@ -34,18 +30,19 @@ model = Model(HiGHS.Optimizer)
 @variable(model, var[1:n, 1:n], Bin)
 # Nie może być połączenia tam gdzie cena == 0, połączenie nie istnieje
 @constraint(model, [i in 1:n, j in 1:n, cost[i, j] == 0], var[i, j] == 0)
+# Wyjście i wejście z każdego node'a może byctylko jedno
+@constraint(model, [i in 1:n], sum(var[i, :]) <= 1)
+@constraint(model, [i in 1:n], sum(var[:, i]) <= 1)
 # Bilans wszystkich pubnktów, zachowanie poprawności ścieżki
 @constraint(model, [i in 1:n], sum(var[i, :]) - sum(var[:, i]) == bilance[i])
 #czas nie może przekraczać wyznaczonego ograniczenia
-@constraint(model, sum(time .* var) <= time_cap)
+@constraint(model, sum(var .* time) <= time_cap)
 
-@objective(model, Min, sum(cost .* var))
+@objective(model, Min, sum(var .* cost))
 
 optimize!(model)
 solution_summary(model)
 
-print("\nKoszt najkrótszej drogi: ", objective_value(model))
-print("Czas najkrótszej drogi: ", value(sum(time .* var)))
-print("Ścieżka: ", value(var))
-
-return time
+println("\nKoszt najkrótszej drogi: ", objective_value(model))
+println("Czas najkrótszej drogi: ", value(sum(time .* var)))
+println("Ścieżka: ", value.(var))
